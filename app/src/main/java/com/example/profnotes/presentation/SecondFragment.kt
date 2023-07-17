e69d4cde4a6df652260562d98aa0b353d197e8a6
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2023-07-17 23:13:09 +0400

    Added splashScreen, added nav_graph

---
+++ app/src/main/java/com/example/profnotes/presentation/SecondFragment.kt
@@ -0,0 +1,51 @@
+package com.example.profnotes.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.profnotes.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var tvTextArgument: TextView
+    private val args by navArgs<SecondFragmentArgs>()
+
+    var recyclerView: RecyclerView? = null
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding: FragmentSecondBinding
+        get() = _binding ?: throw java.lang.RuntimeException("FragmentSecondBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(layoutInflater, container, false)
+        tvTextArgument = binding.tvTextArgument
+        recyclerView = binding.rv
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTextView()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        val items = listOf("item", "item2", "item3")
+        recyclerView?.adapter = RecyclerAdapter(items)
+    }
+
+    private fun setupTextView() {
+        val textFromArgument = args.text
+        tvTextArgument.text = textFromArgument
+    }
+}
